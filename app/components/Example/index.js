29d5d426f3d2e1a62ab755b1a1c298eca37b943b
gabrielmj23 <gabrielmj2312@gmail.com>
2024-12-29 13:04:46 -0400

    chore(docs): remove unused storybook files

--- app/components/Example/index.js
+++
@@ -1,4 +0,0 @@
-// Archivo exportador de componente, para evitar importaciones largas
-// El import quedaria como
-// import Example from './components/Example';
-export { default } from './Example';
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig(() => {
-  const isStorybook = process.env.STORYBOOK === "true";
-
   return {
     css: {
       postcss: {
         plugins: [tailwindcss, autoprefixer],
       },
     },
-    plugins: [
-      react(),
-      tsconfigPaths(),
-      !isStorybook && reactRouter(), // Solo incluye reactRouter si no estás en Storybook
-    ].filter(Boolean), // Elimina los `false` de la lista de plugins
+    plugins: [reactRouter(), tsconfigPaths()],
   };
 });
