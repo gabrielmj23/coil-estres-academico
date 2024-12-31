6671bbddfcea0fab32a0b8a34ea79a85f877b10a
gabrielmj23 <gabrielmj2312@gmail.com>
2024-12-31 18:37:29 -0400

    feat(sisco): show survey options and advance

--- app/components/SurveyOption/SurveyOption.tsx
+++ app/components/SurveyOption/SurveyOption.tsx
@@ -3,27 +3,19 @@ import './SurveyOption.css';
 
 interface SurveyOptionProps {
   options: string[]; // Array of options
-  onChange?: (option: string) => void; // Callback when an option is selected
+  selectedOption: string | null; // Selected option
+  onChange: (option: string) => void; // Callback when an option is selected
 }
 
-export const SurveyOption: React.FC<SurveyOptionProps> = ({ options, onChange }) => {
-  const [selectedOption, setSelectedOption] = useState<string | null >(null);
-
-  const handleSelect = (option: string) => {
-    setSelectedOption(option);
-    if (onChange) {
-      onChange(option);
-    }
-  };
-
+export const SurveyOption: React.FC<SurveyOptionProps> = ({ options, selectedOption, onChange }) => {
   return (
     <div className="survey-option-container w-9/12 sm:w-1/2 md:w-1/3 lg:w-1/4">
-<ul>
+      <ul>
         {options.map((option, index) => (
           <li
             key={index}
             className={`survey-option ${selectedOption === option ? "selected" : ""}`}
-            onClick={() => handleSelect(option)}
+            onClick={() => onChange(option)}
           >
             <span className={`option-label ${selectedOption === option ? "selected" : ""}`}>{option}</span>
             <span className={`radio-circle ${selectedOption === option ? "checked" : ""}`}>
--- app/routes/QuestionnaireSiscoPage.tsx
+++ app/routes/QuestionnaireSiscoPage.tsx
@@ -1,12 +1,81 @@
-import type { Route } from "./+types/questionnaire-sisco";
+// route("cuestionario-sisco", "./routes/QuestionnaireSiscoPage.tsx")
+import type { Route } from "./+types/QuestionnaireSiscoPage";
+import { useQuestionIndex } from "~/hooks/useQuestionIndex";
+import { SurveyOption } from "~/components/SurveyOption/SurveyOption";
+import { redirect } from "react-router";
+import Button from "~/components/ContinueButton/ContinueButton";
+import { useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Cuestionario Sisco" },
-    { name: "description", content: "Responde al cuestionario Sisco." },
+    { title: "Cuestionario SISCO" },
+    { name: "description", content: "Responde al cuestionario SISCO." },
   ];
 }
 
-export default function QuestionnaireSiscoPage() {
-  return <h1>Cuestionario Sisco</h1>;
+export async function loader() {
+  const siscoQuestions = [
+    {
+      id: 1,
+      content: "La competencia con los compañeros del grupo",
+      options: [
+        { id: 1, content: "Nunca", points: 1 },
+        { id: 2, content: "Rara vez", points: 2 },
+        { id: 3, content: "Algunas veces", points: 3 },
+        { id: 4, content: "Casi siempre", points: 4 },
+        { id: 5, content: "Siempre", points: 5 },
+      ],
+    },
+    {
+      id: 2,
+      content: "Sobrecarga de tareas y trabajos escolares",
+      options: [
+        { id: 1, content: "Nunca", points: 1 },
+        { id: 2, content: "Rara vez", points: 2 },
+        { id: 3, content: "Algunas veces", points: 3 },
+        { id: 4, content: "Casi siempre", points: 4 },
+        { id: 5, content: "Siempre", points: 5 },
+      ],
+    },
+  ];
+  return siscoQuestions;
+}
+
+export default function QuestionnaireSiscoPage({
+  loaderData,
+}: Route.ComponentProps) {
+  const [questionIndex, nextQuestion, goBack] = useQuestionIndex();
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+  if (questionIndex < 0 || questionIndex >= loaderData.length) {
+    return redirect("/");
+  }
+
+  const currentQuestion = loaderData[questionIndex];
+
+  return (
+    <>
+      <header className="questionnaire"></header>
+      <main>
+        <div>
+          <h2 className="text-center text-xl font-bold text-coilterracota px-3 py-5 pb-2">
+            {currentQuestion.content}
+          </h2>
+          <SurveyOption
+            options={currentQuestion.options.map((option) => option.content)}
+            selectedOption={selectedOption}
+            onChange={(option) => setSelectedOption(option)}
+          />
+          <Button
+            onClick={() => {
+              console.log(selectedOption);
+              setSelectedOption(null);
+              nextQuestion();
+            }}
+            label="Continuar"
+          />
+        </div>
+      </main>
+    </>
+  );
 }
