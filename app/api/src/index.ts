0c5ecc8c55f4fbabb1c4833143e7968b9ee2470b
karimalisahili <kasahili.20@est.ucab.edu.ve>
2025-01-01 20:50:08 -0400

    initIndicadores()

--- app/api/src/index.ts
+++ app/api/src/index.ts
@@ -1,15 +1,22 @@
 // index.ts
 import express from 'express';
 import routes from './routes';
+import { initIndicadores } from './tables/indicadores';
 
 
 
 const app = express();
 const port = 3000;
 
+
+
 //Middleware
 app.use(express.json());
 app.use('/', routes);
+
+//Inicializador de tablas de la bd
+initIndicadores();
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
--- app/api/src/tables/indicadores.ts
+++ app/api/src/tables/indicadores.ts
@@ -6,3 +6,24 @@ export const indicadores = pgTable("indicadores", {
   nombre: varchar("nombre", { length: 60 }).notNull().unique(),
 });
 
+//inicializador de indicadores
+export const initIndicadores = async () => {
+
+  // Reiniciar la secuencia del ID para que comience desde 1
+  await db.execute(`ALTER SEQUENCE indicadores_id_seq RESTART WITH 1`);
+
+  const predefinedIndicadores = [
+    { nombre: "Estrés Academico"},
+    { nombre: "Ansiedad/Depresión" },
+    { nombre: "Disfunción Social"},
+  ];
+
+  for (const indicador of predefinedIndicadores) {
+    try{
+    await db.insert(indicadores).values(indicador).onConflictDoNothing();
+    console.log(`Indicador ${indicador.nombre} creado`);
+    }catch(e){
+      console.log(e);
+    }
+  } 
+}
