66261108b7edafedea584ebcc32a6fd43a3703c4
hcaraballo2401 <51705435+hcaraballo2401@users.noreply.github.com>
2025-01-03 15:30:23 -0400

    Landing Page

--- app/routes/LandingPage.tsx
+++ app/routes/LandingPage.tsx
@@ -1,4 +1,6 @@
 import type { Route } from "./+types/landing";
+import "./LandingPage.css";
+import PrimaryButton from "../components/PrimaryButton/PrimaryButton";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,5 +10,37 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function LandingPage() {
-  return <h1>Bienvenido a la Página de Inicio</h1>;
+  const handleButtonClick = () => {
+    console.log("activado");
+  };
+
+  return (
+    <div className="welcome-screen">
+      <div className="header">
+        <img src="../public/Project-Logo.png" alt="Logo" className="logo" />
+      </div>
+      <h1 className="title">
+        Cuerpos <span className="highlight"> y mentes</span>
+      </h1>
+      <h2 className="subtitle">en equilibrio</h2>
+      <p className="tagline">
+        "Chequea tu salud, abraza tu{" "}
+        <span className="highlight-green">bienestar</span>"
+      </p>
+      <div className="illustration">
+        <img
+          src="../public/health-home.png"
+          alt="Illustration"
+          className="image"
+        />
+      </div>
+      <PrimaryButton label={"Continuar"} onClick={handleButtonClick} />
+      <p className="login-text">
+        ¿Ya tienes una cuenta?{" "}
+        <a href="/login" className="login-link">
+          Inicia Sesión
+        </a>
+      </p>
+    </div>
+  );
 }
