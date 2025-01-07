a3c2a5097227c6fe28a731af9aa3aafccd1ff4b5
hcaraballo2401 <51705435+hcaraballo2401@users.noreply.github.com>
2025-01-06 22:57:48 -0400

    questionnaireGoldberg

--- app/routes.ts
+++ app/routes.ts
@@ -1,18 +1,18 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("./routes/LandingPage.tsx"), // Ruta principal "/"
+  index("./routes/QuestionnaireSiscoPage.tsx"), // Ruta principal "/"
   route("iniciar-sesion", "./routes/LoginPage.tsx"), // Ruta "/iniciar-sesion"
   route("registrarse", "./routes/RegisterPage.tsx"), // Ruta "/registrarse"
   route("seleccion-de-prueba", "./routes/TestSelectionPage.tsx"), // Ruta "/seleccion-de-prueba"
-  
+
   // Cuestionarios: Sisco y Goldberg
-  route("cuestionario-sisco", "./routes/QuestionnaireSiscoPage.tsx"), // Ruta "/cuestionario-sisco"
+  //route("cuestionario-sisco", "./routes/QuestionnaireSiscoPage.tsx"), // Ruta "/cuestionario-sisco"
   route("cuestionario-goldberg", "./routes/QuestionnaireGoldbergPage.tsx"), // Ruta "/cuestionario-goldberg"
-  
+
   // Completado del cuestionario
   route("completado-cuestionario", "./routes/QuestionnaireCompletionPage.tsx"), // Ruta "/completado-cuestionario"
-  
+
   // Página de recomendaciones y aviso psicológico
   route("recomendaciones", "./routes/RecommendationsPage.tsx"), // Ruta "/recomendaciones"
   route("aviso-psicologico", "./routes/PsychologicalWarningPage.tsx"), // Ruta "/aviso-psicologico"
--- app/routes/QuestionnaireGoldbergPage.tsx
+++ app/routes/QuestionnaireGoldbergPage.tsx
@@ -1,4 +1,12 @@
 import type { Route } from "./+types/QuestionnaireGoldbergPage";
+import { SurveyOption } from "~/components/SurveyOption/SurveyOption";
+import PrimaryButton from "~/components/PrimaryButton/PrimaryButton";
+import { useState } from "react";
+import ArrowRight from "~/icons/ArrowRight";
+import { getPreguntasGoldBerg } from "~/api/controllers/preguntas";
+import { calculateTotalPoints } from "~/api/utils/utils";
+import SectionPage from "~/components/SectionPage/SectionPage";
+import ProgressBar from "../components/ProgressBar/ProgressBar";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,6 +15,100 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function QuestionnaireGoldbergPage() {
-  return <h1>Cuestionario Goldberg</h1>;
+export async function loader() {
+  return getPreguntasGoldBerg();
+}
+
+export default function QuestionnaireGoldbergPage({
+  loaderData,
+}: Route.ComponentProps) {
+  const [sectionIndex, setSectionIndex] = useState(0);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [globalIndex, setGlobalIndex] = useState(0);
+  const [selectedOption, setSelectedOption] = useState<number | null>(null);
+  const [answers, setAnswers] = useState<StoredAnswer[]>([]);
+  const [showInstructions, setShowInstructions] = useState(true);
+
+  const sections = loaderData.sections;
+  const currentSection = sections[sectionIndex];
+  const currentQuestion = currentSection.preguntas[questionIndex];
+
+  const TOTAL_QUESTIONS = 12;
+  /**
+   * Saves an answer and advances to next question/section
+   * @author Gabriel
+   */
+  const saveAnswer = () => {
+    setGlobalIndex(globalIndex + 1);
+    // Store answer
+    setAnswers((prev) => [
+      ...prev,
+      {
+        sectionId: currentSection.idSeccion,
+        questionId: currentQuestion.idPregunta,
+        indicatorId: currentQuestion.idIndicador,
+        optionId: selectedOption || 0,
+        points:
+          currentQuestion.opciones.find((op) => op.idOpcion === selectedOption)
+            ?.puntaje ?? 0,
+      },
+    ]);
+
+    // Clear selection
+    setSelectedOption(null);
+
+    // Continue
+    if (questionIndex + 1 === currentSection.preguntas.length) {
+      if (sectionIndex + 1 === sections.length) {
+        console.log("END");
+        console.log(answers);
+      } else {
+        setSectionIndex((prev) => prev + 1);
+        setQuestionIndex(0);
+        setShowInstructions(true);
+      }
+    } else {
+      setQuestionIndex((prev) => prev + 1);
+    }
+  };
+
+  console.log(calculateTotalPoints(answers));
+
+  if (showInstructions) {
+    return (
+      <SectionPage
+        image={currentSection.imagen}
+        instruction={currentSection.instruccion}
+        onContinue={() => setShowInstructions(false)}
+      />
+    );
+  }
+
+  return (
+    <>
+      <header className="questionnaire"></header>
+      <main>
+        <div>
+          <h2 className="text-center text-xl font-bold text-coilterracota px-3 py-5 pb-2">
+            {currentQuestion.contenido}
+          </h2>
+          <SurveyOption
+            options={currentQuestion.opciones}
+            selectedOption={selectedOption}
+            onChange={(option) => setSelectedOption(option.idOpcion)}
+          />
+          <PrimaryButton
+            label="Continuar"
+            onClick={saveAnswer}
+            disabled={selectedOption === null}
+            icon={<ArrowRight className="w-6" />}
+          />
+
+          <ProgressBar
+            progress={(globalIndex * 100) / TOTAL_QUESTIONS}
+          ></ProgressBar>
+        </div>
+      </main>
+    </>
+  );
 }
