62bd6501a4d4a798a31a1b472d131fe80ef4c643
karimalisahili <kasahili.20@est.ucab.edu.ve>
2025-01-19 16:16:20 -0400

    email field terminado

--- app/components/Field/Field.tsx
+++ app/components/Field/Field.tsx
@@ -1,23 +1,64 @@
-
-import React, { useState } from "react";
 import "./Field.css"; // Archivo CSS para estilos
+import React, {useState} from 'react';
 
-function Field() {
+interface FieldProps {
+  label: string;
+  placeholder: string;
+  type: string; // text || date || number
+  value: string;
+  iconSrc: string;
+  error?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Field: React.FC<{
+  label: string;
+  iconSrc: string;
+  type: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  error: string;
+}> = ({ label, iconSrc, type, value, onChange, placeholder, error }) => {
+
+  const [touched, setTouched] = useState(false);
+
+  const handleBlur = () => {
+    setTouched(true);
+  };
+
+  const handleFocus = () => {
+    setTouched(false);
+  }
   return (
-    <div className="form-container">
-      <label htmlFor="email" className="form-label">
-        Correo Electrónico
+    <div className="mx-auto w-11/12">
+      <label className="block mb-1 font-bold text-coilterracota label-input-style">
+        {label}
       </label>
-      <div className="input-wrapper">
-        <span className="icon">📧</span>
-        <input
-          type="email"
-          id="email"
-          className="email-input"
-          placeholder="gabo@gmail.com"
-        />
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+          <img src={iconSrc} alt="Email Icon" />
+        </div>
+      <input
+        type={type}
+        value={value}
+        onChange={onChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        className={`block w-full ps-12 p-3.5 input-style text-coilgray-light font-bold ${
+          (error != "" && touched)
+            ? 'bg-F7E8E2 border-1 border-coilorange-light field-error'
+            : 'border-none focus:border-coilbeige focus:shadow-md ok-input-style'
+        }`}
+        placeholder={placeholder}
+      />
       </div>
+      {(error != "" && touched) && (
+        <p className="text-center text-size-14 text-coilorange-light">
+          {error}
+        </p>
+      )}
     </div>
   );
-}
+};
 export default Field;
--- app/routes/LoginPage.tsx
+++ app/routes/LoginPage.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router";
 import type { Route } from "./+types/LoginPage";
 import ArrowLeft from "~/icons/ArrowLeft";
 import Field from "~/components/Field/Field";
+import React, { useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,7 +11,27 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const[errorEmail, setErrorEmail] = useState("");
+
+
+  // Función para validar correo electrónico
+  const isValidEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Regex estándar para validar correos
+    return emailRegex.test(email);
+  };
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    if(isValidEmail(email) || email === ""){
+      setErrorEmail("");
+    }else{
+      setErrorEmail("El correo electrónico no tiene un formato válido");
+    }
+    
+  }
   return (
     <div className="h-[100dvh]">
       <header className="questionnaire"></header>
@@ -23,7 +44,15 @@ export default function LoginPage() {
       <main className="flex flex-col gap-5">
         <h1 className="text-3xl text-center">Iniciar Sesión</h1>
 
-        <Field />
+        <Field
+          label="Correo Electrónico"
+          placeholder="example@ucab.com"
+          type="text"
+          onChange={onChangeEmail}
+          value={email}
+          iconSrc="/email-icon.svg"
+          error={errorEmail}
+        />
       </main>
     </div>
   );
