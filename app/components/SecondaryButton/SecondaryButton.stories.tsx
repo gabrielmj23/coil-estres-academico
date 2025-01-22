6f708d19906de571e0f2fc88590565596e66a05d
SantiagoRiveraO <santiago.alonso.rivera@gmail.com>
2025-01-22 10:42:30 -0400

    documentacion

---
+++ app/components/SecondaryButton/SecondaryButton.stories.tsx
@@ -0,0 +1,7 @@
+import type  {Story} from "@ladle/react";
+import SecondaryButton  from "./SecondaryButton";
+
+
+export const SecondaryButtonStory: Story = () => (
+    <SecondaryButton label = {"mbappe"}  />
+) ;
